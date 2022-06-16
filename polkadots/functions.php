6be596001e389ade37e4d3b1f3bdca2286592db0
Jedsadha <Jedsadha1777@users.noreply.github.com>
2022-06-16 12:57:59 +0700

    upload 20220616
    first commit

---
+++ polkadots/functions.php
@@ -0,0 +1,20 @@
+<?php 
+add_theme_support( 'post-thumbnails' );
+
+
+
+
+
+function setup_my_theme() {
+    add_theme_support( 'title-tag' );
+
+ 
+ }
+ add_action( 'after_setup_theme', 'setup_my_theme');
+
+
+
+
+
+
+?>
